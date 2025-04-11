67d6ef1521cf2b17793f809a055832164f7a781c
aissata.sy <aissata.sy@fpublique.gouv.sn>
2025-04-11 16:47:43 +0000

    modifigation_pagination

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -508,9 +508,7 @@
 
           </div>
         </div>
-        <div class="d-flex justify-content-center mt-4">
-    {{ $utilisateurs->links('pagination::bootstrap-5') }}
-</div>
+    
 
         <footer class="footer">
           <div class="container-fluid d-flex justify-content-between">
