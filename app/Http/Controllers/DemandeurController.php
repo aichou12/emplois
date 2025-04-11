65703b9e94c51cfc53ade0b29549936c76abba7a
aissata.sy <aissata.sy@fpublique.gouv.sn>
2025-04-11 12:30:07 +0000

    filtre

--- app/Http/Controllers/DemandeurController.php
+++ app/Http/Controllers/DemandeurController.php
@@ -8,16 +8,35 @@ use Illuminate\Http\Request;
 
 class DemandeurController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Récupérer tous les utilisateurs ayant une inscription complète (relation avec userdata)
-        $utilisateurs = Utilisateur::has('userdata')->paginate(50); // Pagination
-
-
-        return view('admin.liste_demandeur', compact(
-            'utilisateurs',
-          
-          
-        ));
+        $query = Utilisateur::has('userdata'); // Ne récupérer que ceux qui ont une relation userdata
+    
+        // Mapping des champs du formulaire vers les colonnes en BDD
+        $filters = [
+            'id' => 'id',
+            'identity_number' => 'numberid',
+            'username' => 'username',
+            'email' => 'email',
+            'firstname' => 'firstname',
+            'lastname' => 'lastname',
+            'isActif' => 'enabled',
+            'isRecruted' => 'recruted',
+        ];
+    
+        foreach ($filters as $formField => $dbColumn) {
+            if ($request->filled($formField)) {
+                if (in_array($formField, ['isActif', 'isRecruted'])) {
+                    $query->where($dbColumn, $request->input($formField));
+                } else {
+                    $query->where($dbColumn, 'like', '%' . $request->input($formField) . '%');
+                }
+            }
+        }
+    
+        $utilisateurs = $query->paginate(50);
+    
+        return view('admin.liste_demandeur', compact('utilisateurs'));
     }
 }
+    
